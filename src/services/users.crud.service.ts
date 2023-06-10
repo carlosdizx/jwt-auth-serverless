f3bd5d637e8d6e391254a9d360e366be27d5b579
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-06-10 16:09:27 -0500

    :sparkles: create service for business logic from users

---
+++ src/services/users.crud.service.ts
@@ -0,0 +1,34 @@
+import UserDao from "../dao/user.dao";
+import RoleDao from "../dao/role.dao";
+import responseLambda from "../utils/response";
+
+export default class UsersCrudService {
+  public static createUser = async ({
+    email,
+    password,
+    rolesIds,
+  }: {
+    email: string;
+    password: string;
+    rolesIds: string[];
+  }) => {
+    try {
+      const userFound = await UserDao.findByEmail(email);
+      if (userFound)
+        return responseLambda(409, { message: "User already exists" });
+      const roles = await RoleDao.findRolesByIds(rolesIds);
+      if (roles.length === 0)
+        return responseLambda(400, { message: "Roles are not found" });
+      const user = await UserDao.create(email, password, roles);
+      return responseLambda(200, {
+        message: "User created successfully",
+        email,
+        roles: roles.map(role => role.description),
+        id: user.id,
+      });
+    } catch (error) {
+      console.error(error);
+      return responseLambda(500, { message: "Error creating user, verify properties", error });
+    }
+  };
+}
